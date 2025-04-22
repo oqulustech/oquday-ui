748ad89478c4a6463bb65e88783fae98e1b2c86f
raviGanapuram <raviraj.ganapuram97@gmail.com>
2025-04-22 22:01:36 +0530

    commit to pull the changes

---
+++ src/components/common/navigation.component.tsx
@@ -0,0 +1,9 @@
+import { Row, Col } from "react-bootstrap"
+export default function NavHeader() {
+    return (
+        <Row>
+            <Col className="col-6"> <h2> This is Navigation Part</h2 ></Col>
+            <Col className="col-6"> <h2> This is Navigation Part</h2 ></Col>
+        </Row>
+    )
+}
---
+++ src/constant/constant.tsx
@@ -0,0 +1,2 @@
+export const serverPath = 'https://localhost:4200/';
+export const login = '/login';
